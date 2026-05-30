c4a84ccc6f041f874977eba280c3c8b148abfa1c
BladimirIgnazenko <ysdo4259@gmail.com>
2026-05-30 23:55:05 +0900

    feat(fe): フロントエンド環境構築およびナビゲーション基盤の実装

--- fe_mobile/babel.config.js
+++ fe_mobile/babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
